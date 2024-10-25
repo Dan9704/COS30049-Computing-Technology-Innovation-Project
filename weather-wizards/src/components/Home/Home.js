b842fd2f50958cae798531cb73a3e7964a0d93ac
Dan9704 <duongcrseven@gmail.com>
2024-10-25 11:20:46 +1100

    Update Home.js

--- weather-wizards/src/components/Home/Home.js
+++ weather-wizards/src/components/Home/Home.js
@@ -3,8 +3,6 @@ import { Container, Row, Col } from "react-bootstrap";
 import homeLogo from "../../Assets/avatar1.png";
 import Tilt from "react-parallax-tilt";
 import Particle from "../Particle";
-import Home2 from "./Home2";
-import Type from "./Type";
 
 function Home() {
   return (
@@ -12,34 +10,7 @@ function Home() {
       <Container fluid className="home-section" id="home">
         <Particle />
         <Container className="home-content">
-          <Row>
-            <Col md={7} className="home-header">
-            <h1 style={{ paddingBottom: 15 }} className="heading">
-              G'Day, Mate!{" "}
-              <span className="kangaroo">🦘</span>
-            </h1>
-
-              <h1 className="heading-name">
-                I'm
-                <strong className="main-name"> Danny Nguyen</strong>
-              </h1>
-
-              <div style={{ padding: 50, textAlign: "left" }}>
-                <Type />
-              </div>
-            </Col>
-
-            <Col md={5} style={{ paddingBottom: 20 }}>
-              <Tilt>
-                <img
-                  src={homeLogo}
-                  alt="home pic"
-                  className="img-fluid"
-                  style={{ maxHeight: "450px" }}
-                />
-              </Tilt>
-            </Col>
-          </Row>
+          
         </Container>
       </Container>
       <Home2 />
