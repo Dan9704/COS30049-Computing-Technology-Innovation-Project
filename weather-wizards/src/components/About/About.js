9f9b126466af44e11c8da1a43cf8b9f317b9e3c7
Dan9704 <duongcrseven@gmail.com>
2024-10-25 12:13:54 +1100

    upddate

--- weather-wizards/src/components/About/About.js
+++ weather-wizards/src/components/About/About.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Particle from "../Particle";
+import homeLogo from "../../Assets/danny_picture.png";
+import Tilt from "react-parallax-tilt";
 
 function About() {
   return (
@@ -8,7 +10,86 @@ function About() {
       {/* Particle background effect  */}
       <Particle />
       <Container>
-        
+      <Row>
+            <Col md={7} className="home-header">
+            <h1 style={{ paddingBottom: 15 }} className="heading">
+              G'Day, Mate!{" "}
+              <span className="kangaroo">🦘</span>
+            </h1>
+
+              <h1 className="heading-name">
+                I'm
+                <strong className="main-name"> Danny Nguyen</strong>
+              </h1>
+            </Col>
+
+            <Col md={5} style={{ paddingBottom: 20 }}>
+              <Tilt>
+                <img
+                  src={homeLogo}
+                  alt="home pic"
+                  className="img-fluid"
+                  style={{ maxHeight: "450px" }}
+                />
+              </Tilt>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col md={7} className="home-header">
+            <h1 style={{ paddingBottom: 15 }} className="heading">
+              G'Day, Mate!{" "}
+              <span className="kangaroo">🦘</span>
+            </h1>
+
+              <h1 className="heading-name">
+                I'm
+                <strong className="main-name"> Danny Nguyen</strong>
+              </h1>
+            </Col>
+
+            <Col md={5} style={{ paddingBottom: 20 }}>
+              <Tilt>
+                <img
+                  src={homeLogo}
+                  alt="home pic"
+                  className="img-fluid"
+                  style={{ maxHeight: "450px" }}
+                />
+              </Tilt>
+            </Col>
+          </Row>
+
+
+
+          <Row>
+            <Col md={7} className="home-header">
+            <h1 style={{ paddingBottom: 15 }} className="heading">
+              G'Day, Mate!{" "}
+              <span className="kangaroo">🦘</span>
+            </h1>
+
+              <h1 className="heading-name">
+                I'm
+                <strong className="main-name"> Danny Nguyen</strong>
+              </h1>
+            </Col>
+
+            <Col md={5} style={{ paddingBottom: 20 }}>
+              <Tilt>
+                <img
+                  src={homeLogo}
+                  alt="home pic"
+                  className="img-fluid"
+                  style={{ maxHeight: "450px" }}
+                />
+              </Tilt>
+            </Col>
+          </Row>
+
+
+
+
       </Container>
     </Container>
   );
--- weather-wizards/src/components/Home/Home.js
+++ weather-wizards/src/components/Home/Home.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import homeLogo from "../../Assets/danny_picture.png";
 import Particle from "../Particle";
+// import Type from "./Type";
+import Tilt from "react-parallax-tilt";
 
 function Home() {
   return (
@@ -8,7 +11,30 @@ function Home() {
       <Container fluid className="home-section" id="home">
         <Particle />
         <Container className="home-content">
-          
+        <Row>
+            <Col md={7} className="home-header">
+            <h1 style={{ paddingBottom: 15 }} className="heading">
+              G'Day, Mate!{" "}
+              <span className="kangaroo">🦘</span>
+            </h1>
+
+              <h1 className="heading-name">
+                I'm
+                <strong className="main-name"> Danny Nguyen</strong>
+              </h1>
+            </Col>
+
+            <Col md={5} style={{ paddingBottom: 20 }}>
+              <Tilt>
+                <img
+                  src={homeLogo}
+                  alt="home pic"
+                  className="img-fluid"
+                  style={{ maxHeight: "450px" }}
+                />
+              </Tilt>
+            </Col>
+          </Row>
         </Container>
       </Container>
     </section>
