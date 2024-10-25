04e82ff525de18fb43f38da6dd4efe24875c4f58
Dan9704 <duongcrseven@gmail.com>
2024-10-25 11:14:39 +1100

    updaet

--- weather-wizards/src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Header() {
-    return (
-        <div className="app-header">
-            <h1>Weather Wizards Application</h1>
-        </div>
-    );
-    }
